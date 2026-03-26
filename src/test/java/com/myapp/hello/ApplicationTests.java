101f04e24b434e6120551746598b2d8d4ccb79ae
techmahato-com <techmahato@gmail.com>
2026-03-26 11:15:27 +0000

    Secuirity check updated

--- src/test/java/com/myapp/hello/ApplicationTests.java
+++ src/test/java/com/myapp/hello/ApplicationTests.java
@@ -3,15 +3,13 @@ package com.myapp.hello;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 @SpringBootTest
 class ApplicationTests {
 
     @Test
     void contextLoads() {
-    }
-
-    @Test
-    void mainMethodRuns() {
-        Application.main(new String[] {});
+        assertNotNull(Application.class);
     }
 }
